2def0235e665ac6813fb68260e509c178f312035
4015b11 <4015b11@corazon.ece.uprm.edu>
2016-08-31 10:14:50 -0400

    Frame size has been changed to be smaller and relocated to the center of the screen. It was also renamed, now it utilizes the users name. (Lines 7 and 8 where the ones with mayor changes.)

--- F16_CIIC4010_Lab3/src/Lab3Exercises.java
+++ F16_CIIC4010_Lab3/src/Lab3Exercises.java
@@ -2,10 +2,10 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Hello, world!");
-		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(200, 200);
-		myFrame.setSize(400, 100);
-		myFrame.setVisible(true);
+		JFrame Anel = new JFrame("Hello, world!");
+		Anel.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		Anel.setLocation(850, 400);
+		Anel.setSize(200, 200);
+		Anel.setVisible(true);
 	}
 }
