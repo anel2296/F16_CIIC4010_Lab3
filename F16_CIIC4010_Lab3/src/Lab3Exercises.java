98d7e8d755cd29f80e6c313cdfec2c25706a0bff
4015b11 <4015b11@corazon.ece.uprm.edu>
2016-08-31 10:24:16 -0400

    This time I'll commit and push... let's see what happens!

--- F16_CIIC4010_Lab3/src/Lab3Exercises.java
+++ F16_CIIC4010_Lab3/src/Lab3Exercises.java
@@ -2,10 +2,10 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame Anel = new JFrame("Hello, world!");
-		Anel.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		Anel.setLocation(850, 400);
-		Anel.setSize(200, 200);
-		Anel.setVisible(true);
+		JFrame AnelMartinez = new JFrame("Hello, world!");
+		AnelMartinez.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		AnelMartinez.setLocation(850, 400);
+		AnelMartinez.setSize(200, 200);
+		AnelMartinez.setVisible(true);
 	}
 }
